fdaf73315f875b7b12580e785847b201a255c075
lizyn <26119970+lizyn@users.noreply.github.com>
2022-10-13 03:45:43 -0400

    decouple modal and new post button

--- photofolio/src/App.js
+++ photofolio/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import React, { useState } from 'react';
 import FollowRecommendation from './components/FollowRecommendation';
 import CreatePostModal from './components/CreatePostModal';
 
 function App() {
+  const [postModalOpen, setPostModalOpen] = useState(false);
+  const closePostModal = () => setPostModalOpen(false);
   return (
     <div>
       <FollowRecommendation/>
       <div>
-        <CreatePostModal/>
+        <button type="button" className="button" onClick={() => setPostModalOpen(o => !o)}>
+          New Post
+        </button>
+        <CreatePostModal closeModal={closePostModal} open={postModalOpen}/>
       </div>
     </div>
   );
