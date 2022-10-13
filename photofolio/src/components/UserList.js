c04b6a0658ab7520dd142d1304eaaa6cf59678c5
lizyn <26119970+lizyn@users.noreply.github.com>
2022-10-13 16:15:27 -0400

    Update create post modal style with MUI

--- photofolio/src/components/UserList.js
+++ photofolio/src/components/UserList.js
@@ -5,7 +5,7 @@ function UserList(props) {
   let userRows = [];
   const users = props.users;
   users.forEach((user, index) => {
-    userRows.push(<div className='user-row-card' key={index}><UserRow profilePicUrl={user.profilePicUrl} name={user.name} ring={props.ring} showFollow={props.showFollow}/></div>);
+    userRows.push(<div className='user-row-card' key={index}><UserRow profilePicUrl={user.profilePicUrl} name={user.name} {...props}/></div>);
   })
   return (   
       <div>
