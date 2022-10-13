60dd889650f9808993b014f4aa3755d0fb59283c
lizyn <26119970+lizyn@users.noreply.github.com>
2022-10-13 18:24:24 -0400

    Add links to navbar

--- photofolio/src/components/Navbar.js
+++ photofolio/src/components/Navbar.js
@@ -5,17 +5,33 @@ import homeIcon from '../icons/Home.svg'
 import newIcon from '../icons/New.svg'
 import profileIcon from '../icons/Person.svg'
 import logoutIcon from '../icons/Arrow_Export.svg'
-function Navbar () {
-    return(
-        <nav className="Navbar">
-            <ul>
-                <li><img src={logoutIcon} alt='logout'></img>Log Out</li>
-                <li><img src={profileIcon} alt='profile'></img>Profile</li>
-                <li><img src={newIcon} alt='new'></img>New Post</li>
-                <li><img src={homeIcon} alt='home'></img>Home</li>
-            </ul>
-        </nav>
-    )
+function Navbar(props) {
+  return (
+    <Router>
+      <nav className="Navbar">
+        <ul>
+          <li>
+            <Link to={`/logout`}>
+              <img src={logoutIcon} alt='logout'></img>Log Out
+            </Link>
+          </li>
+          <li>
+            <Link to={`/profile`}>
+              <img src={profileIcon} alt='profile'></img>Profile
+            </Link>
+          </li>
+          <li>
+            <img src={newIcon} alt='new' onClick={() => props.setPostModalOpen(o => !o)}></img>New Post
+          </li>
+          <li>
+            <Link to={`/home`}>
+              <img src={homeIcon} alt='home'></img>Home
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </Router>
+  )
 }
 
 
