8c666fe97ac6ebcd8ed495d22bdd774b52267259
emshg <emshg@seas.upenn.edu>
2022-10-21 11:02:23 -0400

    update feedlist.jsx
    fetch data and populate feeds in feedlist component

--- photofolio/src/components/FeedList.jsx
+++ photofolio/src/components/FeedList.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Feed from './Feed';
 
 function FeedList() {
-  return (
-    <div>
-      <Feed />
-      <Feed />
-    </div>
-  );
+  const [posts, setPosts] = useState([]);
+  const postId = useRef(0);
+  const firstRendering = useRef(true);
+
+  useEffect(() => {
+    // get the list of students from the backend
+    async function fetchData() {
+      const postsData = await fetchPosts();
+      setPosts(postsData);
+    }
+    // only load data on the first rendering or
+    // when a new post is created
+    if (firstRendering.current) {
+      firstRendering.current = false;
+      fetchData();
+    }
+  });
+
+  const postsList = posts;
+  const populateFeeds = () => {
+    const feeds = [];
+    postsList.forEach((post) => {
+      feeds.push(
+        <Feed title={post.text} img={post.photo.src} key={postId.current} />
+      );
+      postId.current += 1;
+    });
+    return feeds;
+  };
+
+  const feeds = populateFeeds();
+
+  return <div>{feeds}</div>;
 }
 
 export default FeedList;
