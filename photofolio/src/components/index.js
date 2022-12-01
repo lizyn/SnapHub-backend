c72dd6ea311fac7a2da24c2cfb45116d6ffdec2e
emshg <emshg@seas.upenn.edu>
2022-12-01 14:08:22 -0500

    Update index.js
    So index.js can be used to run the server

--- photofolio/src/components/index.js
+++ photofolio/src/components/index.js
@@ -1,19 +1,19 @@
-// const webapp = require('./server');
+const webapp = require('./server');
 
-// const port = 8080;
+const port = 8080;
 
-// webapp.listen(port, async () => {
-//   console.log(`Server running on port: ${port}`);
-// });
-const { closeMongoDBConnection, getDB } = require('./dbConnection');
+webapp.listen(port, async () => {
+  console.log(`Server running on port: ${port}`);
+});
+// const { closeMongoDBConnection, getDB } = require('./dbConnection');
 
-// let mongo;
-let db;
+// // let mongo;
+// let db;
 
-const main = async () => {
-  db = await getDB();
-  const result = await db.collection('posts').deleteMany({ text: 'test' });
-  console.log(result);
-};
+// const main = async () => {
+//   db = await getDB();
+//   const result = await db.collection('posts').deleteMany({ text: 'test' });
+//   console.log(result);
+// };
 
-main();
+// main();
