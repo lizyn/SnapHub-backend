e99fec29fe882748b772d7450ef69243742e1460
lizyn <26119970+lizyn@users.noreply.github.com>
2022-12-01 17:15:53 -0500

    s3 apis

---
+++ photofolio/src/components/s3manips.js
@@ -0,0 +1,82 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+// The access ID and secret key of the S3 bucket
+const keys = require('./keys');
+
+const { ID, SECRET } = keys;
+
+// The name of the bucket that you have created
+const BUCKET_NAME = 'cis557fall22';
+
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
+
+// upload a file
+const uploadFile = (file) => {
+  const fileContent = fs.readFileSync(file.filepath);
+
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: file.originalFilename, // File name we want to upload
+    Body: fileContent
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+    return '1123';
+  });
+};
+
+// uploadFile('cat.jpg');
+
+// read a file
+const readFile = async (fileName) => {
+  // Setting up S3 read parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName // File name we want to retrieve
+  };
+
+  // download file from the bucket
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    // do something with the file
+    const fStream = fs.createWriteStream(`1${fileName}`);
+    fStream.write(data.Body);
+    fStream.end();
+
+    console.log(`File downloaded successfully. ${fileName}`);
+  });
+};
+
+// readFile('cat.jpg');
+
+// delete a file
+const deleteFile = async (fileName) => {
+  // Setting up S3 delete parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName // File name we want to delete
+  };
+
+  // download file from the bucket
+  s3.deleteObject(params, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`File deleted successfully. ${data}`);
+  });
+};
+
+// deleteFile('cat.jpg');
+
+module.exports = { uploadFile, readFile, deleteFile };
